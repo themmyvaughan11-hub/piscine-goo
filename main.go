d508b58bb46edec4219a165b9dd948397010aa19
themmyvaughan11-hub <themmyvaughan11@gmail.com>
2026-02-11 05:19:27 -0500

    My very first clean commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Write a function that a string as an argument and returns (true) if the string contains any number otherwise return (false)")
+}
